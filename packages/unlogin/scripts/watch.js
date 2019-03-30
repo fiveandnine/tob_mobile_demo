4cd22661fcbba620b6c3d3eb0975463a697dfaf6
xiaolei <lei.xiao@ifchange.com>
2019-03-30 21:26:42 +0800

    chore: 增加watch.js控制台打印文字的颜色

--- packages/unlogin/scripts/watch.js
+++ packages/unlogin/scripts/watch.js
@@ -1,21 +1,34 @@
 const chokidar = require('chokidar');
 const exec = require('child_process').exec;
-const path = require('path')
+const path = require('path');
+const colors = require('colors');
+colors.setTheme({
+  silly: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'bgRed',
+  success: 'bgGreen',
+});
 // One-liner for current directory, ignores .dotfiles
 chokidar.watch(path.resolve(__dirname, '../src')).on('all', (event, _path) => {
-  if (event === "change") {
-    console.log(event, _path);
-    builder()
+  if (event === 'change') {
+    console.log(event.input, _path);
+    builder();
   }
 });
 
 function builder() {
-  exec('pkm build', { cwd: path.resolve(__dirname, '../../../') }, function (error, stdout, stderr) {
+  exec('pkm build', { cwd: path.resolve(__dirname, '../../../') }, function(error, stdout, stderr) {
     if (error) {
-      console.error('error: ' + error);
+      console.error('error: '.bgRed + error);
       return;
     }
-    console.log('stdout success: ' + stdout);
+    console.log('stdout success: '.success + stdout);
   });
 }
-
